9c96e9c87956a6e380655b947c301e7da531239d
Ahmed Shebl <hamadam21@gmail.com>
2023-04-03 14:13:16 +0200

    Add files via upload

---
+++ insert_data.sh
@@ -0,0 +1,61 @@
+#! /bin/bash
+
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+
+# Do not change code above this line. Use the PSQL variable above to query your database.
+echo $($PSQL "TRUNCATE teams, games")
+
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do 
+  if [[ $WINNER != "winner" ]]
+  then
+  # WINNER
+    # get WINNER team_id
+      TEAM_ID_WINNER=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+
+      # if not found
+      if [[ -z $TEAM_ID_WINNER ]]
+      then
+        # insert WINNER team
+        INSERT_TEAM_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+        if [[ $INSERT_TEAM_RESULT == "INSERT 0 1" ]]
+        then
+          echo Inserted into teams, $WINNER
+        fi
+
+        # get new WINNER team_id
+        TEAM_ID_WINNER=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+      fi
+
+# OPPONENT
+      # get OPPONENT team_id
+      TEAM_ID_OPPONENT=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+
+      # if not found
+      if [[ -z $TEAM_ID_OPPONENT ]]
+      then
+        # insert OPPONENT team
+        INSERT_TEAM_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
+        if [[ $INSERT_TEAM_RESULT == "INSERT 0 1" ]]
+        then
+          echo Inserted into teams, $OPPONENT
+        fi
+
+        # get new OPPONENT team_id
+        TEAM_ID_OPPONENT=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+      fi
+
+      INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $TEAM_ID_WINNER, $TEAM_ID_OPPONENT, $WINNER_GOALS, $OPPONENT_GOALS)")
+      if [[ $INSERT_GAME_RESULT == "INSERT 0 1" ]]
+        then
+          echo Inserted into games, $WINNER:$OPPONENT
+        fi
+  fi
+
+  
+done
